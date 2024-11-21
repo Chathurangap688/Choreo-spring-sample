18b58b2bf9fa8997eb29bcae6c0f07f42002c8d9
chathurangap689 <v-chathuranga.priyad@shrss.com>
2024-11-22 03:25:25 +0530

    remoe cors

--- src/main/java/com/chathuranga/sample/SampleApplication.java
+++ src/main/java/com/chathuranga/sample/SampleApplication.java
@@ -2,6 +2,7 @@ package com.chathuranga.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class SampleApplication {
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
