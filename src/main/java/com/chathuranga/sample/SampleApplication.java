6a709f2f641cfe2a38230b6a31f8b9ee295ae2c3
chathurangap689 <v-chathuranga.priyad@shrss.com>
2024-11-25 01:59:37 +0530

    Add corss

--- src/main/java/com/chathuranga/sample/SampleApplication.java
+++ src/main/java/com/chathuranga/sample/SampleApplication.java
@@ -2,10 +2,13 @@ package com.chathuranga.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -20,4 +23,13 @@ public class SampleApplication {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
+    @Configuration
+    public class WebConfiguration implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedMethods("*");
+        }
+    }
+
 }
